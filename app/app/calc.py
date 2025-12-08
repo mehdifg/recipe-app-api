a1806b78e535317912c13251a96c10f2f3b24c37
mehdi <mehdi.fgaier@esprit.tn>
2025-12-08 16:14:24 +0100

    postgres dependencies and tests

---
+++ app/app/calc.py
@@ -0,0 +1,9 @@
+"""
+Docstring for recipe-app-api.app.app.calc
+"""
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return y - x
---
+++ app/app/tests.py
@@ -0,0 +1,16 @@
+"""
+Docstring for recipe-app-api.app.app.tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """test the calc module"""
+    def test_add_numbers(self):
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
+    def test_substract_numbers(self):
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, 5)
