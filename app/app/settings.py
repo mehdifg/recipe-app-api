4f86ae608c6789a6f53c571518a30b296ce2047f
mehdi <mehdi.fgaier@esprit.tn>
2025-12-09 16:22:28 +0100

    user api added fixed

--- app/app/settings.py
+++ app/app/settings.py
@@ -136,4 +136,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.User'
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
 }
